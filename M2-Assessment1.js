5c1ca77f3ab73aa3869520f05352bee01b92afed
nokuthu <104618152+nokuthu@users.noreply.github.com>
2022-09-18 17:28:02 +0200

    Add files via upload

---
+++ M2-Assessment1.js
@@ -0,0 +1,6 @@
+var firstname = 'Nokuthula';
+var surname = ' matlaila';
+var institution = 'MTN Academy';
+var username = 'nokuthu'
+
+console.log ("my name is" + firstname + "a student from" + institution + " and my Github username is" + username)
